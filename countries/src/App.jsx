e1a57e9a8b39b926afe52dd94e0e124ed5e3b58f
Hanane <hanane.elkaaba1@gmail.com>
2023-11-07 16:38:50 +0100

    Display countries with their flags

---
+++ countries/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {countries.map((country, index) => (
+        <div className="container" key={index}>
+          <img
+            src={country.flags.png}
+            alt={country.name.common}
+            className="image-flag"
+          />
+          <p key={index}>{country.name.common}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
